e171c81258180cead18e026985641811b06a6dfb
Edwin Garcia <160422256+Edwin4156@users.noreply.github.com>
2026-05-30 22:43:20 -0600

    Add files via upload

---
+++ Sistema de Farmacia/Sistema-de-Farmacia/src/main/java/com/example/Sistema/de/Farmacia/SistemaDeFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema.de.Farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeFarmaciaApplication.class, args);
+	}
+
+}
